078cdff14cda8491b9464394f7243d6290ba38fb
Eran Stiller <git@stiller.co.il>
2016-10-19 12:39:10 +0300

    Add message send from reader to queue

--- reader/reader.py
+++ reader/reader.py
@@ -15,8 +15,9 @@ class TwitterConfiguration:
 
 
 class MyStreamer(TwythonStreamer):
-    def __init__(self, twitter_configuration):
+    def __init__(self, twitter_configuration, pika_channel):
         super().__init__(twitter_configuration.consumer_key, twitter_configuration.consumer_secret, twitter_configuration.access_token, twitter_configuration.access_token_secret)
+        self.pika_channel = pika_channel
 
     def on_success(self, data):
         if 'text' in data:
@@ -24,27 +25,27 @@ class MyStreamer(TwythonStreamer):
                 'text': data['text']
             }
             serialized_data = json.dumps(output_data)
+            self.pika_channel.basic_publish('', 'tweets', serialized_data)
             print(serialized_data)
 
     def on_error(self, status_code, data):
         print(status_code)
 
 
-pika_connection = None
-while pika_connection == None:
+connection = None
+while connection == None:
     try:
-        pika_connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
     except pika.exceptions.ConnectionClosed:
         print("RabbitMQ connection still closed. Retrying.")
         time.sleep(5)
-
-pika_channel = pika_connection.channel()
-pika_channel.queue_declare(queue='tweets')
+channel = connection.channel()
+channel.queue_declare(queue='tweets')
 
 configuration = TwitterConfiguration()
 while True:
     try:
-        stream = MyStreamer(configuration)
+        stream = MyStreamer(configuration, channel)
         stream.statuses.filter(track='trump,clinton')
     except:
         print("Unexpected error: ", sys.exc_info()[0])
