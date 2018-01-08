6d30a7e03fe2ce15e88656b3246ff41715383aca
Eran Stiller <git@stiller.blog>
2018-01-08 09:00:04 +0200

    Refine log

--- analyzer/src/Analyzer/QueueBase.cs
+++ analyzer/src/Analyzer/QueueBase.cs
@@ -15,7 +15,7 @@ namespace Analyzer
 
             var policy = Policy
                 .Handle<RabbitMQ.Client.Exceptions.BrokerUnreachableException>()
-                .WaitAndRetryForever(i => TimeSpan.FromSeconds(5), (ex, time) => Console.WriteLine("Unable to connect to broker. Retrying."));
+                .WaitAndRetryForever(i => TimeSpan.FromSeconds(5), (ex, time) => Console.WriteLine($"Unable to connect to broker with hostname '{hostName}'. Retrying."));
             IConnection conn = policy.Execute(() => factory.CreateConnection());
             Channel = conn.CreateModel();
             Channel.QueueDeclare(queueName, false, false, false, null);
